92d63c632930cf170e5d49dcb54e99a93c7fecbc
Arden <arden.haldorson@gmail.com>
2022-12-13 15:07:45 +0100

    copy in

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const nameValue = document.querySelector('#name');
 
+const mail = document.querySelector('#mail');
+
+const picture = document.querySelector('#picture');
+
+const inputSubmit = document.querySelector('#submit');
+
+const comfirmAction = document.querySelector('#submit-comfirmed');
+
+
+
+
+
+inputSubmit.addEventListener('click', (e) => {
+
+    console.log('hej');
+
+e.preventDefault()
+
+
+
+comfirmAction.innerHTML= `<p>
+
+You have sent in this information; <br>
+
+Name:${nameValue.value} <br>
+
+Mail:${mail.value}</p> <br>
+
+Image:${picture.value}
+
+</p>`
+
+}
+
+);
